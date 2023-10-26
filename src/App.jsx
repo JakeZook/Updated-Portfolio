1c1dcc50bd6f2c1b131ac921b9ace2af2fb88863
Jake Zook <jakezook01@gmail.com>
2023-10-25 21:48:04 -0600

    Basic file structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./components/Nav";
+
+function App() {
+	return (
+		<>
+			<Nav />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+export default function Nav() {
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<a href="">BIO</a>
+					</li>
+					<li>
+						<a href="">Projects</a>
+					</li>
+					<li>
+						<a href="">Resume</a>
+					</li>
+					<li>
+						<a href="">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
