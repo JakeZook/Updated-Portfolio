07a4580519a5c25d82de3a6fb212c2237f62b6c3
Jake Zook <jakezook01@gmail.com>
2023-10-27 02:30:05 -0600

    Finished adding content to landing page and styled elemnts.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,105 @@
 export default function Nav() {
 	return (
-		<nav className="navbar navbar-expand-lg fixed-top">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="/">
-					Jake Zook
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNavAltMarkup"
-					aria-controls="navbarNavAltMarkup"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<div className="navbar-nav ms-auto text-center">
-						<a
-							className="nav-link active hoverable"
-							aria-current="page"
-							href="/"
-						>
-							Home
-						</a>
-						<a className="nav-link hoverable" href="/Projects">
-							Projects
-						</a>
-						<a className="nav-link hoverable" href="#">
-							Contact
-						</a>
-						<a className="nav-link hoverable" href="#">
-							Resume
-						</a>
+		<div className="fixed-top">
+			<nav className="navbar navbar-expand-lg d-flex">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="/">
+						Jake Zook
+					</a>
+					<div className="text-center d-flex justify-content-center m-2">
+						<div className="badge d-flex">
+							<a href="" className="border rounded-circle">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									fill="currentColor"
+									className="bi bi-linkedin"
+									viewBox="0 0 16 16"
+								>
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+								</svg>
+							</a>
+						</div>
+						<div className="badge d-flex">
+							<a href="" className="border rounded-circle">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									fill="currentColor"
+									className="bi bi-github"
+									viewBox="0 0 16 16"
+								>
+									<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+								</svg>
+							</a>
+						</div>
+						<div className="badge d-flex">
+							<a href="" className="border rounded-circle">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									fill="currentColor"
+									className="bi bi-slack"
+									viewBox="0 0 16 16"
+								>
+									<path d="M3.362 10.11c0 .926-.756 1.681-1.681 1.681S0 11.036 0 10.111C0 9.186.756 8.43 1.68 8.43h1.682v1.68zm.846 0c0-.924.756-1.68 1.681-1.68s1.681.756 1.681 1.68v4.21c0 .924-.756 1.68-1.68 1.68a1.685 1.685 0 0 1-1.682-1.68v-4.21zM5.89 3.362c-.926 0-1.682-.756-1.682-1.681S4.964 0 5.89 0s1.68.756 1.68 1.68v1.682H5.89zm0 .846c.924 0 1.68.756 1.68 1.681S6.814 7.57 5.89 7.57H1.68C.757 7.57 0 6.814 0 5.89c0-.926.756-1.682 1.68-1.682h4.21zm6.749 1.682c0-.926.755-1.682 1.68-1.682.925 0 1.681.756 1.681 1.681s-.756 1.681-1.68 1.681h-1.681V5.89zm-.848 0c0 .924-.755 1.68-1.68 1.68A1.685 1.685 0 0 1 8.43 5.89V1.68C8.43.757 9.186 0 10.11 0c.926 0 1.681.756 1.681 1.68v4.21zm-1.681 6.748c.926 0 1.682.756 1.682 1.681S11.036 16 10.11 16s-1.681-.756-1.681-1.68v-1.682h1.68zm0-.847c-.924 0-1.68-.755-1.68-1.68 0-.925.756-1.681 1.68-1.681h4.21c.924 0 1.68.756 1.68 1.68 0 .926-.756 1.681-1.68 1.681h-4.21z" />
+								</svg>
+							</a>
+						</div>
+						<div className="badge d-flex">
+							<a href="" className="border rounded-circle">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									fill="currentColor"
+									className="bi bi-envelope"
+									viewBox="0 0 16 16"
+								>
+									<path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+								</svg>
+							</a>
+						</div>
+					</div>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavAltMarkup"
+						aria-controls="navbarNavAltMarkup"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+						<div className="navbar-nav ms-auto text-center">
+							<a
+								className="nav-link active hoverable"
+								aria-current="page"
+								href="/"
+							>
+								Home
+							</a>
+							<a className="nav-link hoverable" href="/Bio">
+								Bio
+							</a>
+							<a className="nav-link hoverable" href="/Projects">
+								Projects
+							</a>
+							<a className="nav-link hoverable" href="#">
+								Contact
+							</a>
+							<a className="nav-link hoverable" href="#">
+								Resume
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,57 @@
 import "./Home.css";
+import TopProjects from "../components/TopProjects";
 
 export default function Home() {
 	return (
-		<div className="row d-flex justify-content-center">
-			<div className="col-3" id="header">
-				<h1>Jake Zook</h1>
-				<h2 className="mt-3 mb-3">Full Stack Web Developer</h2>
-				<p id="header-bio">
-					Recent graduate of DU Full Stack Dev Bootcamp. proficient in numerous
-					languages and technologies. Game dev on the side.
-				</p>
-			</div>
-			<div className="col-2"></div>
-			<div className="col-3 d-flex text-center justify-content-around align-items-center flex-column">
-				<div className="circular-profile-pic mx-auto">
-					<img src="../../Assets/pp.PNG" alt="" />
+		<div>
+			<div className="row d-flex justify-content-center mt-5 mb-5 pb-5">
+				<div className="col-lg-3 col-sm-12" id="header">
+					<h1>Jake Zook</h1>
+					<h2 className="mt-3 mb-3">Full Stack Web Developer</h2>
+					<p id="header-bio">
+						Recent graduate of DU Full Stack Dev Bootcamp. proficient in
+						numerous languages and technologies. Game dev on the side.
+					</p>
 				</div>
-				<a href="/bio" className="transparent-button m-2">
-					Learn More
-				</a>
+				<div className="col-lg-3 col-sm-12 d-flex text-center justify-content-around align-items-center flex-column">
+					<div className="circular-profile-pic mx-auto">
+						<img src="../../Assets/pp.PNG" alt="Picture of Jake Zook" />
+					</div>
+					<a href="/bio" className="transparent-button m-2">
+						Learn More
+					</a>
+				</div>
 			</div>
+			<div className="row d-flex justify-content-center mt-5 mb-5 pb-5">
+				<div className="col-lg-3 col-sm-12 d-flex text-center justify-content-around align-items-center flex-column">
+					<div className="circular-profile-pic mx-auto">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="128"
+							height="128"
+							fill="currentColor"
+							className="bi bi-gear-wide-connected"
+							viewBox="0 0 16 16"
+						>
+							<path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z" />
+						</svg>
+					</div>
+					<a href="/bio" className="transparent-button m-2">
+						All Projects
+					</a>
+				</div>
+				<div className="col-lg-3 col-sm-12 text-end" id="header-projects">
+					<h1>Projects</h1>
+					<h2 className="mt-3 mb-3">Check out my work</h2>
+					<p id="header-bio">
+						Projects created during bootcamp as well as some passion projects
+					</p>
+				</div>
+			</div>
+			<div className="d-flex justify-content-center mt-5">
+				<h1 id="projects-table-header">Top Projects</h1>
+			</div>
+			<TopProjects />
 		</div>
 	);
 }
